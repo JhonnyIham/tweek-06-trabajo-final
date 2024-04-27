5b94bad7805963c690357ca89575578c4e345647
Jhonny Torres <iham1405.jitv@gmail.com>
2024-04-26 23:02:01 -0500

    7 commit

--- src/models/index.js
+++ src/models/index.js
@@ -2,6 +2,7 @@ const User = require ('./User')
 const Category = require('./Category')
 const Product = require('./Product')
 const Cart = require('./Cart')
+const Purchase = require('./Purchase')
 
 // Product -> categoryId
 Product.belongsTo(Category)
@@ -12,3 +13,10 @@ User.hasMany(Cart)
 
 Cart.belongsTo(Product)
 Product.hasMany(Cart)
+
+Purchase.belongsTo(User)
+User.hasMany(Purchase)
+
+Purchase.belongsTo(Product)
+Product.hasMany(Purchase)
+
